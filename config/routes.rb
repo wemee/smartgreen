595a95c5f157a9ebc8802bddacc3a8eade5cbb06
wemee <wemee7012@gmail.com>
2015-09-11 17:10:03 +0800

    首頁改為學校列表

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'welcome#index'
+  root 'schools#index'
   post '/', :to => 'welcome#login'
   
   # Example of regular route:
